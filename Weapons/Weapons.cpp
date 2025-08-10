d29ff06839a9109ef11f3f52cc5a5d4e19eadc1d
RusProgger <ruslantimka92@gmail.com>
2025-08-11 00:25:59 +0300

    weapons random state

--- Weapons/Weapons.cpp
+++ Weapons/Weapons.cpp
@@ -1,1 +1,25 @@
-﻿
+﻿#include <iostream>
+#include <random>
+
+void WeaponsStateAK74() {
+	std::cout << "Cостояние АК74: ";
+}
+
+
+int RandomWeaponsState() {
+	std::random_device rd;                        
+	std::mt19937 gen(rd());                       
+	std::uniform_int_distribution<> dist(1, 100); 
+	return dist(gen);
+}
+
+
+
+int main() {
+
+	int randomNumber = RandomWeaponsState();
+
+
+	std::cin.get();
+	return 0;
+}
