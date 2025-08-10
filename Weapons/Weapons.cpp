818a228398916096455539ec51cf42e892df846e
RusProgger <ruslantimka92@gmail.com>
2025-08-11 00:39:21 +0300

    weapons random state

--- Weapons/Weapons.cpp
+++ Weapons/Weapons.cpp
@@ -17,8 +17,30 @@ int RandomWeaponsState() {
 
 int main() {
 
+	// Локаль
+	std::locale::global(std::locale(""));
+	
+	// Рандом от 1 до 100
 	int randomNumber = RandomWeaponsState();
 
+	// Проверка оружия на состояние
+
+
+	if (randomNumber >= 80 && randomNumber <= 100) {
+		WeaponsStateAK74();
+		std::cout << "Отличное состояние: " << randomNumber << std::endl;
+	}
+	else if (randomNumber >= 50 && randomNumber <= 79) {
+		WeaponsStateAK74();
+		std::cout << "Состояние среднее: " << randomNumber << std::endl;
+		
+	}
+	else {
+		WeaponsStateAK74();
+		std::cout << "Требуется ремонт: " << randomNumber << std::endl;
+	}
+
+	
 
 	std::cin.get();
 	return 0;
