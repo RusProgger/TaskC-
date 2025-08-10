3ea822c01f86692aef7ef514f89e2d9c6755cc99
RusProgger <ruslantimka92@gmail.com>
2025-08-10 16:57:52 +0300

    vector

---
+++ 1/Vector_1/Vector_1.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <vector>
+
+
+
+int main() {
+	std::locale::global(std::locale(""));
+	
+	int vectorUser{ 0 };
+
+	std::vector<int> userV;
+
+	std::cout << "Введите кол-во цифр: ";
+	std::cin >> vectorUser;
+
+	// цикл попыток 
+
+	for (int i = 0; i < vectorUser; i++) {
+		int index;
+		std::cin >> index;
+		userV.push_back(index);
+	}
+
+	for (int i = 0; i < vectorUser; i++) {
+		std::cout <<  userV[i] << std::endl;
+	}
+
+
+	std::cin.get();
+	return 0;
+}
