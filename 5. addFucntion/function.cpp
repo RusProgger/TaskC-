c96bfbc22dc76a2c6cbd3a5cd9b5e2253e37feac
RusProgger <ruslantimka92@gmail.com>
2025-08-13 16:10:56 +0300

    addFuction

---
+++ 5. addFucntion/function.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+/*
+    5 - Написать функцию которая принимает положительных числа: а и b, затем вывести результат
+*/
+
+unsigned int myAdd(int a, int b) {
+    
+    if(a > b) {
+        std::cout << a << std::endl;
+    } else {
+        std::cout << b << std::endl;
+    }
+}
+
+int main() {
+
+    
+}
