237ab8c38321f4c366d7389d80a0c7196bd046f3
RusProgger <ruslantimka92@gmail.com>
2025-08-14 19:11:38 +0300

    locale winApi

--- locale_WinApi/locale_Api.cpp
+++ locale_WinApi/locale_Api.cpp
@@ -1,14 +1,17 @@
 #include <iostream>
 #include <windows.h>
 
+
 void localeWinApi() {
     SetConsoleOutputCP(1251); // Устанавливает кодовую страницу вывода
     SetConsoleCP(1251);       // Устанавливает кодовую страницу ввода
 }
+
 
 int main() {
 
     localeWinApi();
     std::cout << "Привет, это вызов функции localeWinApi." << std::endl;
 
+    
 }
