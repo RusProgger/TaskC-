8b0be16c6163018503bc843d6dd0629ff1b843dd
RusProgger <ruslantimka92@gmail.com>
2025-08-15 17:19:17 +0300

    Even and Odd number

--- 07/main.cpp
+++ 07/main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 
+/*
+    --- Пользователь вводит целое число. Определите, чётное оно или нечётное. ---
+*/
+
 int main() {
 
-    
+    int number {0};
+    std::cout << "Enter number: ";
+    std::cin >> number;
+
+    if(number % 2 == 0) {
+        std::cout << "Even number = " << number;
+    } else {
+        std::cout << "Odd number = " << number;
+    }
+
 }
