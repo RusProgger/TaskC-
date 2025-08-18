1ae54a4fc6b2a0e648fba5ae8a31669ed8cc29a0
RusProgger <ruslantimka92@gmail.com>
2025-08-18 16:33:12 +0300

    Конвертер бит в байт

---
+++ Converter-bit to byte/Converter-bit to byte.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+
+void localeWinApi() {
+    SetConsoleOutputCP(1251); 
+    SetConsoleCP(1251);       
+}
+
+int main() {
+    
+    // локаль 
+    localeWinApi();
+
+
+    return 0;
+}
+
