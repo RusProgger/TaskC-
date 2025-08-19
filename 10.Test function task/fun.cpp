8eca64f7759fd9c6b1f0f7b74bca199049851125
RusProgger <ruslantimka92@gmail.com>
2025-08-20 01:09:23 +0300

    Добавлена новая задача

--- 10.Test function task/fun.cpp
+++ 10.Test function task/fun.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 
+int Test(int a);
+float Test(float b);
+std::string Test(std::string text);
+
 int main() {
-    
+
+    int a = 10;
+    float b = 2.5;
+    std::string name = "Function";
+
+    int resFT = Test(a);
+
+
+
     return 0;
 }
