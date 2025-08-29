5bbd05a499373ec43119142266a8a7f9d8afad88
RusProgger <ruslantimka92@gmail.com>
2025-08-29 12:34:19 +0300

    Добавлена новая задача

--- 19. Summa N/main.cpp
+++ 19. Summa N/main.cpp
@@ -3,17 +3,19 @@
 int main() {
 
     int number;
+    int summa = 0;
 
     std::cout << "Enter number: ";
     std::cin >> number;
 
     if(number > 0) {
         for(int n = 0; n < number; n++) {
-            std::cout << number;
+           summa += n;
         }
     } else {
         std::cout << "Not a number";
     }
 
+    std::cout << "Summa " << summa << " = " << number << std::endl;
     return 0;
 }
