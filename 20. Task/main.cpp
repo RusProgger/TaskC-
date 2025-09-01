0ff9277a5dbb9854c68433bd60e9d1bf575efb74
RusProgger <ruslantimka92@gmail.com>
2025-09-01 16:34:45 +0300

    Добавлена новая здача

--- 20. Task/main.cpp
+++ 20. Task/main.cpp
@@ -3,9 +3,8 @@
 int main() {
 
     int pin;
-    std::cout << "Enter pin code: ";
-
     do {
+        std::cout << "Enter pin code: ";
         std::cin >> pin;
         if(pin != 555) {
             std::cout << "Incorrect pin code\n";
