80d1740678ca67481aabdde05c3eca640add1d67
RusProgger <ruslantimka92@gmail.com>
2025-09-17 02:21:50 +0300

    Добавлено решение  задачи

---
+++ 24. AgeAndDrivingLicense/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+
+int main() {
+    int age;
+    std::cin >> age;
+
+    // Проверка условия на возраст
+
+    if(age >= 18) {
+    
+    std::cout << "Ура! Можно получать права!" << std::endl;
+    } else {
+    
+    std::cout << "Права можно получить только с 18 лет!" << std::endl;
+    }
+
+}
