3be374e178db5c436570f06d7358c2e8ef4639c5
RusProgger <ruslantimka92@gmail.com>
2025-09-26 12:21:38 +0300

    Добавлено новое решение в файл bip2, ввод числа

---
+++ 28. Task-Bipp/bip2.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+
+int main() {
+
+    int forMax;
+
+    // Спрашиваем у пользователя ввод числа
+
+    std::cout << "Enter number: ";
+    std::cin >> forMax;
+
+    std::string beep = "Бип!!";
+
+    // цикл 
+
+    for(int i = 1; i <= forMax; i++) {
+        // вывод цикла 
+        std::cout << i << "\n";
+
+        // проверка условий
+
+        if(i % 5 == 0) {
+            std::cout << beep << "\n";
+        }
+    }
+
+    return 0;
+}
