f418059c43d8220ba560ca925f4df6f358f987f8
RusProgger <ruslantimka92@gmail.com>
2025-09-28 16:02:00 +0300

    Убрана проверка из задания

--- 21. ForArrs/forArrs.cpp
+++ 21. ForArrs/forArrs.cpp
@@ -10,16 +10,11 @@ int main() {
 
     int* arrMax = new int[userArr];
 
-    if(userArr < 0) {
-        std::cout << "Error.\n";
-        return 1;
-    } else {
-
-        for(int i = 0; i < userArr; i++) {
-            std::cin >> arrMax[i];
-            std::cout << std::endl;
-        }
+    for(int i = 0; i < userArr; i++) {
+        std::cin >> arrMax[i];
+        std::cout << std::endl;
     }
+    
 
     for(int j = 0; j < userArr; j++ ) {
         std::cout << "Element arrry - " << arrMax[j] << " " << "\n";
