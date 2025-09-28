6f9ec56f1fcdb9db219389166a3c2d947b25b881
RusProgger <ruslantimka92@gmail.com>
2025-09-28 15:58:38 +0300

    Добавлено еще одно решение в 21 задаче

--- 21. ForArrs/forArrs.cpp
+++ 21. ForArrs/forArrs.cpp
@@ -2,8 +2,31 @@
 
 int main() {
 
-    
+    unsigned int userArr;
 
+    std::cout << "Enter element: ";
+
+    std::cin >> userArr;
+
+    int* arrMax = new int[userArr];
+
+    if(userArr < 0) {
+        std::cout << "Error.\n";
+        return 1;
+    } else {
+
+        for(int i = 0; i < userArr; i++) {
+            std::cin >> arrMax[i];
+            std::cout << std::endl;
+        }
+    }
+
+    for(int j = 0; j < userArr; j++ ) {
+        std::cout << "Element arrry - " << arrMax[j] << " " << "\n";
+    }
+
+    delete[] arrMax;
+    
 
     return 0;
 }
