d5482bfa94ca0e93c61a4f9e93d25a56856c59c1
RusProgger <ruslantimka92@gmail.com>
2025-12-07 08:48:16 +0200

    Решение задачи "факториал числа"

--- 30. FuctorialNumber/fuctorialNumber.cpp
+++ 30. FuctorialNumber/fuctorialNumber.cpp
@@ -6,7 +6,18 @@ int main() {
 
     std::locale::global(std::locale());
 
-    
+    int n = 0; // хранит число, которое ввёл пользователь.
+    int res = 1; // результат факториала
+    int count = 1; // счётчик
+
+    // цикл 
+
+    while(count <= n) {
+        res = res * count;
+        count = count + 1;
+    }
+
+    std::cout << "FuctorialNumber: " << res;
 
     std::cin.get();
     return 0;
