2002b2c7358fc4dfb72acfda6d48c54052e18c97
RusProgger <ruslantimka92@gmail.com>
2025-12-07 14:02:36 +0200

    Решение 30 задачи "Факториал числа функция"

--- 30. FuctorialNumber/fuctoriaFunction.cpp
+++ 30. FuctorialNumber/fuctoriaFunction.cpp
@@ -2,8 +2,19 @@
 
 // Функция факториал числа 
 
+int fact(int n) {
+    if(n == 0) {
+        return 1;
+    }
+
+    return n * fact(n - 1);
+}
+
 int main() {
 
+    int n;
+    std::cin >> n;
+    std::cout << fact(n);
 
 
     return 0;
