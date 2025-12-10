3b3c1473bd5e176d21dcfc16a0f756c8831f538e
RusProgger <ruslantimka92@gmail.com>
2025-12-10 15:28:36 +0200

    Переписываем код "Улучшенная версия"

--- 06. locale_WinApi/locale_Api.cpp
+++ 06. locale_WinApi/locale_Api.cpp
@@ -43,7 +43,7 @@ int main() {
     // первый способ, нужно подключить макрос, Ошибка возникает из-за конфликта имён max, потому что в windows.h определён макрос, #define NOMINMAX
     std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
     // Второй способ не всегда актуальный, лучше использовать 1 способ
-    std::cin.ignore((std::numeric_limits<std::streamsize>::max)(), '\n');
+    //std::cin.ignore((std::numeric_limits<std::streamsize>::max)(), '\n');
     std::cin.get();
     return 0;
 }
