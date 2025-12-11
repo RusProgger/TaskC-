012e248292d4074c45b29837c1c268156203016b
RusProgger <ruslantimka92@gmail.com>
2025-12-12 00:14:42 +0200

    Работа с классами в С++, добавлен bool

--- 31. classPerson/class.cpp
+++ 31. classPerson/class.cpp
@@ -23,11 +23,12 @@ class User {
     public:
     std::string city;
     unsigned int addres;
-    bool isMale;
+    bool married;
 
     void UserInfo() {
         std::cout <<"User city: " << city << std::endl;
         std::cout <<"User adress" << addres << std::endl;
+        // std::cout << "User is married: "
     }
 };
 
