c1fb67c091a30835a1de1363d080f2be7d94e073
RusProgger <ruslantimka92@gmail.com>
2025-12-14 03:15:53 +0200

    Программа для вывода статистики дня

--- 17. DayAndHours/Day.c
+++ 17. DayAndHours/Day.c
@@ -7,6 +7,14 @@ int main() {
 
     unsigned short timeDay = 0;
 
+    printf("Enter number time: ");
+
+    scanf_s("%d", &timeDay);
+
+    if(timeDay >= 0 && timeDay <= 3) {
+        printf("Night\n");
+    }
+
 
     return 0;
 }
