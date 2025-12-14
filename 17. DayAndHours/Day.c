3b50c2e39b3238477ba68b67ae7e836ac524201d
RusProgger <ruslantimka92@gmail.com>
2025-12-14 03:19:32 +0200

    Программа для вывода статистики дня

--- 17. DayAndHours/Day.c
+++ 17. DayAndHours/Day.c
@@ -13,6 +13,12 @@ int main() {
 
     if(timeDay >= 0 && timeDay <= 3) {
         printf("Night\n");
+    } else if(timeDay >= 4 && timeDay <= 11) {
+        printf("Morning");
+    } else if(timeDay >= 11 && timeDay <= 16) {
+        printf("Day");
+    } else if(timeDay >= 17 && timeDay <= 23) {
+        printf("Evening");
     }
 
 
