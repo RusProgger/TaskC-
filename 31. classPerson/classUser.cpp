d0339feb9ab93154748ffc7f7a52355f586f1328
RusProgger <ruslantimka92@gmail.com>
2025-12-15 02:03:18 +0200

    Работа с классами С++ "Практика"

--- 31. classPerson/classUser.cpp
+++ 31. classPerson/classUser.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
+#include <vector>
+
+// Cоздадим класс User
+
+class User {
+
+    // Имя пользователя user;
+    std::string name;
+
+    // Возраст пользователя 
+
+    unsigned short age{};
+
+
+};
+
+
 
 int main() {
-    
+
     return 0;
 }
