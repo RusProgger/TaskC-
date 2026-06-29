3318c89db76723c1fd876ac311335eefff4a0b42
RusProgger <ruslantimka92@gmail.com>
2026-06-29 08:43:21 +0300

    Add task Sum of Digits of a Three-Digit Number

--- 32. SumofDigitsofaThreeDigitNumber/main.cpp
+++ 32. SumofDigitsofaThreeDigitNumber/main.cpp
@@ -3,8 +3,16 @@
 
 int main() {
 
+    int number {};
+    std::print("Enter number: ");
+    std::cin >> number;
 
-
+    int one = (number / 100);
+    int hundreds = (number / 10) % 10;
+    int tens = (number % 10);
+    // summa
+    int result = (one + hundreds + tens);
+    
     std::cin.get();
     return 0;
 }
