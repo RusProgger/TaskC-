aed71c10996db3d9ead422b8481105bb5c0b055d
RusProgger <ruslantimka92@gmail.com>
2026-07-27 13:59:56 +0300

    33. Task Convert string to uppercase

--- 33. ConvertStringtoUppercase/main.cpp
+++ 33. ConvertStringtoUppercase/main.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
 #include <cctype>
+#include <string>
+
+
 
 int main() {
 
-    
+    std::string text {};
+    std::cout << "Enter text: ";
+    std::getline(std::cin, text);
+
+    // проверка если строка пустая то выводим сообщение о ошибке
+
+    if(text.empty()) {
+        std::cout << "String is empty!\n";
+        return 1;
+    }
+
     return 0;
 }
