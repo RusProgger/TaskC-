65c5b2e64c63b817786aac2593f46754f7fef728
RusProgger <ruslantimka92@gmail.com>
2026-07-29 21:30:10 +0300

    Update task

--- 32. SumofDigitsofaThreeDigitNumber/main.cpp
+++ 32. SumofDigitsofaThreeDigitNumber/main.cpp
@@ -10,6 +10,12 @@ int main() {
 
     // обработка ошибки
 
+    if(std::cin.fail()) {
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        std::cout << "Ошибка!! Это не число..\n";
+    }
+
     int one = (number / 100);
     int hundreds = (number / 10) % 10;
     int tens = (number % 10);
